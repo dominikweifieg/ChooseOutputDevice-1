d2af29af090c7f97e5684f07e374c53e8bc3cca6
Dominik Wei-Fieg <dominik.weifieg@gmail.com>
2012-10-04 19:31:42 +0200

    Initial Commit

---
+++ ChooseOutputDevice/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ChooseOutputDevice
+//
+//  Created by Dominik Wei-Fieg on 04.10.12.
+//  Copyright (c) 2012 Ars Subtilior. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
